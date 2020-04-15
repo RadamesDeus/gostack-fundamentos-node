0282d3507e4f98f56e3b55349bc2b1a91ec44a3b
Radames Deus <raihard@gmail.com>
2020-04-14 21:37:34 -0300

    desafio feito

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,55 @@ interface Balance {
   total: number;
 }
 
+interface Parmscreate {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+
+    this.balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): {} {
+    this.getBalance();
+    const data = { transactions: this.transactions, balance: this.balance };
+    return data;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance: Balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+
+    this.transactions.map(transaction => {
+      balance.income += transaction.type === 'income' ? transaction.value : 0;
+      balance.outcome += transaction.type === 'outcome' ? transaction.value : 0;
+      return balance;
+    }, 0);
+
+    balance.total += balance.income - balance.outcome;
+    this.balance = balance;
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: Parmscreate): Transaction {
+    const transaction = new Transaction({ title, type, value });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
